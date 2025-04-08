27f270d24becded77d333bd65448d734c349da48
Camila Huamani <jyb990106@gmail.com>
2025-04-08 17:44:19 -0500

    feat(main): add main behavior

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.Aggregates;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+var supplierAddress = new Address("123 Main St", "12345","Springfield", "IL", "62701", "USA");
+var supplier = new Supplier("SUP123", "ACME Supplies", supplierAddress);
+
+// Create a purchase object
+var purchaseOrder = new PurchaseOrder("PO001", new SupplierID(supplier.Identifier), "USD");
+purchaseOrder.AddItem(ProductId.New(), 10, 25.99m);
+purchaseOrder.AddItem(ProductId.New(), 5, 15.99m);
+
+// Display purchase order details
+Console.WriteLine($"Purchase Order ID: {purchaseOrder.OrderNumber} created on {purchaseOrder.OrderDate} for supplier {supplier.Name}");
+foreach (var item in purchaseOrder.Items)
+{
+    var itemSubtotal = item.CalculateSubtotal();
+    Console.WriteLine($"Item Subtotal: {itemSubtotal.AsString()}");
+}
+var total = purchaseOrder.CalculateTotal();
+Console.WriteLine($"Total: {total.AsString()}");
